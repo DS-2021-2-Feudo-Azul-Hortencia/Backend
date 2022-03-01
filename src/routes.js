9313c3a3423c8f857a61dc23e08dfb845d847a91
Joberth Rogers <joberth.rogers18@gmail.com>
2022-03-01 14:06:32 -0400

    refactor: modify structure from project

---
+++ src/routes.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const userController = require('./controllers/user');
+const route = express.Router();
+
+route.get("/", (req, res) => {
+    res.json({ hello: "world" });
+});
+
+route.get('/users', userController.getUsers);
+
+route.post('/users', userController.createUser);
+
+module.exports = route;
--- src/server.js
+++ src/server.js
@@ -1,12 +1,19 @@
-const express = require('express');
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require('cors');
+const routes = require('./routes')
+require('./config/db');
 
 const PORT = process.env.PORT || 3000;
 const app = express();
 
-app.get("/", (req, res) => {
-    res.json({ hello: 'world'});
-});
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(cors({origin: '*'}));
+
+app.use(routes)
 
 app.listen(PORT, () => {
-    console.log(`Server running at port ${PORT}`);
+  console.log(`Server running at port ${PORT}`);
 });
